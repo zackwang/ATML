c2727c6c1560d9121367883505e3ef13385839db
Zack Wang <zack.zq.wang@gmail.com>
2022-04-03 16:55:08 +0100

    add pubmed load function

--- data.py
+++ data.py
@@ -15,13 +15,13 @@ def encode_labels(label_strings):
     
     return np.array(encoded_labels, dtype=np.int32)
 
-def build_feature_matrix(content):
-    features_mtx = sp.csr_matrix(content[:, 1:-1], dtype=np.float32)
+def row_normalize(mtx):
+    # normalize feature matrix by each row
+    r_sum = np.array(mtx.sum(axis=1))
+    r_sum_mtx = r_sum * np.ones(mtx.shape)
+    normalized_feature_mtx = np.divide(mtx.todense(), r_sum_mtx)
 
-    r_sum = np.array(features_mtx.sum(axis=1))
-    r_sum_mtx = r_sum * np.ones(features_mtx.shape)
-
-    return np.divide(features_mtx.todense(), r_sum_mtx)
+    return normalized_feature_mtx
 
 def build_adj_matrix(edges, nodes_idx_map):
     to_nodes = []
@@ -55,7 +55,7 @@ def load_cora():
     cites = np.genfromtxt(f"{path}/cora.cites", dtype=np.int32)
 
     nodes = np.array(content[:, 0], dtype=np.int32)
-    features = build_feature_matrix(content)
+    features = row_normalize(features_mtx = sp.csr_matrix(content[:, 1:-1], dtype=np.float32))
     labels = encode_labels(content[:, -1])
 
     # build graph
@@ -83,7 +83,7 @@ def load_citeseer():
     cites = np.genfromtxt(f"{path}/citeseer.cites", dtype=np.dtype(str))
 
     nodes = np.array(content[:, 0], dtype=np.dtype(str))
-    features = build_feature_matrix(content)
+    features = row_normalize(features_mtx = sp.csr_matrix(content[:, 1:-1], dtype=np.float32))
     labels = encode_labels(content[:, -1])
 
     # build graph
@@ -104,8 +104,92 @@ def load_citeseer():
 
     return adj_mtx, features, labels, idx_train, idx_val, idx_test
 
+
+def build_pubmed_features(path):
+    with open(f"{path}/Pubmed-Diabetes.NODE.paper.tab") as f:
+        lines = f.readlines() # skip first line
+
+        # skip first line
+        lines.pop(0)
+
+        # build feature dict
+        line = lines.pop(0)
+        all_words = line.split()
+        all_words = all_words[1:-1]
+
+        feature_idx_map = {} # feature to idx mapping
+        for i, token in enumerate(all_words):
+            word = token.split(':')[1]
+            feature_idx_map[word] = i
+        
+        row = 0
+        nodes_idx_map = {}
+        label_strings = []
+        feature_matrix = sp.lil_matrix((len(lines), len(feature_idx_map)))
+
+        # e.g. 3542527	label=2	w-use=0.027970030654407077	w-studi=0.013917168664368762	summary=w-use,w-studi
+
+        for line in lines:
+            tokens = line.split()
+
+            node = tokens[0]
+            assert(node not in nodes_idx_map)
+            nodes_idx_map[node] = row
+            
+            label = tokens[1].split('=')[1]
+            label_strings.append(label)
+
+            features = tokens[2:-1]
+            for feature in features:
+                fname, fvalue = feature.split('=')
+                feature_matrix[(row, feature_idx_map[fname])] = float(fvalue)
+
+            row += 1
+        
+        features = row_normalize(feature_matrix)
+        labels = encode_labels(label_strings)
+
+        return features, labels, nodes_idx_map
+
+
+def build_pubmed_adj_matrix(path, nodes_idx_map):
+    with open(f"{path}/Pubmed-Diabetes.NODE.cites.tab") as f:
+        lines = f.readlines() # skip first line
+
+        # skip first two lines
+        lines.pop(0)
+        lines.pop(0)
+
+        edges = []
+        # e.g. 33824	paper:19127292	|	paper:17363749
+        for line in lines:
+            tokens = line.split()
+            from_node = tokens[1].split(':')[1]
+            to_node = tokens[2].split(':')[1]
+            edges.append([to_node, from_node])
+
+        return build_adj_matrix(edges, nodes_idx_map)
+
+
 def load_pubmed():
-    pass
+    path = './data/test'
+
+    features, labels, nodes_idx_map = build_pubmed_features(path)
+    adj_mtx = build_pubmed_adj_matrix(path, nodes_idx_map)
+
+    idx_train = range(140)
+    idx_val = range(200, 500)
+    idx_test = range(500, 1500)
+
+    features = torch.FloatTensor(features)
+    labels = torch.LongTensor(labels)
+
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+
+    return adj_mtx, features, labels, idx_train, idx_val, idx_test
+
 
 def load_dataset(dataset):
     if dataset == 'cora':
